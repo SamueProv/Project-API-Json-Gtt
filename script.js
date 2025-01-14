5bcf25374647286738e41ffa4e4ee1b337f16f04
SamuPr. <provaroni.samuele@itisgrassi.edu.it>
2025-01-14 11:33:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
 let URL = "https://gpa.madbob.org/query.php?stop=";
 
-function aggiungiPassaggio(linea, orario){
+function aggiungiPassaggio(linea, orari){
     let div = document.createElement("div");
-    let p1 = document.createElement("p");
-    let p2 = document.createElement("p");
-    p1.innerHTML = linea;
-    p2.innerHTML = orario;
-    div.appendChild(p1);
-    div.appendChild(p2);
+    let cerchio = document.createElement("div");
+    let orariDiv = document.createElement("div");
+
+    cerchio.classList.add("cerchio");
+    cerchio.innerHTML = linea;
+
+    orariDiv.classList.add("orari");
+    orari.forEach(orario => {
+        let p = document.createElement("p");
+        p.classList.add("orario");
+        p.innerHTML = orario.hour + (orario.realtime ? " *" : "");
+        orariDiv.appendChild(p);
+    });
+
+    div.appendChild(cerchio);
+    div.appendChild(orariDiv);
     div.classList.add("col");
-    document.getElementById("lista").appendChild(div);    
+    return div;
 }
 
-function mostra(lista){
-    aggiungiPassaggio(lista[0].line, lista[0].hour);
-    aggiungiPassaggio(lista[1].line, lista[1].hour);
+function mostra(array){
+    let lista = document.getElementById("lista");
+    lista.innerHTML = '';
+
+    let linee = {};
+    array.forEach(element => {
+        if (!linee[element.line]) {
+            linee[element.line] = [];
+        }
+        linee[element.line].push({ hour: element.hour, realtime: element.realtime });
+    });
+
+    for (let linea in linee) {
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+        rowDiv.appendChild(aggiungiPassaggio(linea, linee[linea]));
+        lista.appendChild(rowDiv);
+
+        let br = document.createElement("br");
+        lista.appendChild(br);
+    }
+
+    let text = document.getElementById("num").value;
+    if (isNaN(text)) {
+        alert("Inserire un numero valido");
+    } else if (array.length == 0) {
+        alert("Fermata non trovata");
+    }
 }
 
-function cercafermata(){
-fetch(URL + document.getElementById("num").value)
-.then(x => x.json())
-.then(y => mostra(y));
+function cercaFermata(){
+    fetch(URL + document.getElementById("num").value)
+    .then(response => response.json())
+    .then(data => mostra(data))
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Errore nel recupero dei dati');
+    });
 }
